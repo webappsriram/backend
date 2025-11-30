c77f3b7396158865a56dc70b7026b69ac10d7786
Nithin <nithin@Nithins-MacBook-Pro.local>
2025-11-30 14:40:51 +0530

    DB hosted

--- src/config/database.js
+++ src/config/database.js
@@ -9,6 +9,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'invo_db',
   waitForConnections: true,
+  port: process.env.DB_PORT || 3306,
   connectionLimit: 10,
   queueLimit: 0,
   enableKeepAlive: true,
